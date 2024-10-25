00bc9d894e66049b1725256887dfe63e5d5faba6
gianttelepathicsquid <kevinxrp@gmail.com>
2024-10-25 10:15:24 -0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import InventoryExplainer from './components/InventoryExplainer';
+
+function App() {
+  return (
+    <div style={{ 
+      minHeight: '100vh',
+      backgroundColor: '#0A192F',
+      padding: '20px'
+    }}>
+      <InventoryExplainer />
+    </div>
+  );
+}
+
+export default App;
